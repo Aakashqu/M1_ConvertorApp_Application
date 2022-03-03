e142d4501219c49f2df2c18517f3bb2178235966
Aakashqu <75572777+Aakashqu@users.noreply.github.com>
2022-03-04 00:31:41 +0530

    Update convertor.h

--- 3_Implementation/include/convertor.h
+++ 3_Implementation/include/convertor.h
@@ -1,2 +1,13 @@
+/**
+ * @file convertor.c
+ * @author Aakash Maurya (aakashmaurya166@gmail.com)
+ * @brief Convertor Application in C
+ * @version 0.1
+ * @date 2022-03-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include<stddef.h>
 #include<stdio.h>
